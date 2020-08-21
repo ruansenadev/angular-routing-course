b496685e407346b9170bd16a2fe60b0ca064691e
RuanSena <ruansenadev@gmail.com>
2020-08-21 14:11:38 -0300

    404 wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules, UrlSerializer } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(routes, {
 
+    })
   ],
   exports: [RouterModule],
   providers: [
