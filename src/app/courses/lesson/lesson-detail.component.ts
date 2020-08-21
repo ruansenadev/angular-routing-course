f390b5fe911c625c6829eef629b74ccc20f75b20
RuanSena <ruansenadev@gmail.com>
2020-08-21 18:02:24 -0300

    programmatic navigation

--- src/app/courses/lesson/lesson-detail.component.ts
+++ src/app/courses/lesson/lesson-detail.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {LessonDetail} from "../model/lesson-detail";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { LessonDetail } from "../model/lesson-detail";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'lesson',
@@ -13,16 +13,22 @@ export class LessonDetailComponent implements OnInit {
 
   lesson$: Observable<LessonDetail>;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
 
     console.log("Created LessonDetailComponent...");
 
   }
 
   ngOnInit() {
+    console.log(this.route.data)
     this.lesson$ = this.route.data.pipe(map(data => data['lesson']))
   }
 
-
+  previous(seq: number): void {
+    this.router.navigate(['lessons', seq - 1], { relativeTo: this.route.parent })
+  }
+  next(seq: number): void {
+    this.router.navigate(['lessons', seq + 1], { relativeTo: this.route.parent })
+  }
 
 }
