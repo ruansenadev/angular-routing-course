244f48b0e482311f63f07344c7f908cc17008815
RuanSena <ruansenadev@gmail.com>
2020-08-22 10:58:32 -0300

    prevent load guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule, PreloadAllModules, UrlSerializer } from '@angular
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { CoursesLoadGuard } from "./courses/courses-load.guard";
 
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule),
+    canLoad: [CoursesLoadGuard]
+  },
   { path: '**', component: PageNotFoundComponent }
 ];
 
@@ -21,7 +25,7 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
   providers: [
-
+    CoursesLoadGuard
   ]
 })
 export class AppRoutingModule {
