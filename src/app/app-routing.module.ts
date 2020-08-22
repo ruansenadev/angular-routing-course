26e1e23399e92f053d79dc98c9390fe1cd910468
RuanSena <ruansenadev@gmail.com>
2020-08-22 11:58:46 -0300

    custom preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CoursesLoadGuard } from "./courses/courses-load.guard";
+import { PreloadingStrategyModules } from "./services/preloading.strategy";
 
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
@@ -11,7 +12,10 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   {
     path: 'courses',
-    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule),
+    data: {
+      preload: true
+    }
     // canLoad: [CoursesLoadGuard]
   },
   { path: '**', component: PageNotFoundComponent }
@@ -20,12 +24,13 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules
+      preloadingStrategy: PreloadingStrategyModules
     })
   ],
   exports: [RouterModule],
   providers: [
-    CoursesLoadGuard
+    PreloadingStrategyModules
+    // CoursesLoadGuard
   ]
 })
 export class AppRoutingModule {
