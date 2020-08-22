560faa3f1b2fe50c0a9ffb7251884bc356a7f51b
RuanSena <ruansenadev@gmail.com>
2020-08-22 10:36:35 -0300

    activate child implement fix

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, UrlTree, CanActivateChild } from '@angular/router';
 import { AuthStore } from "./auth.store";
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private router: Router, private authService: AuthStore) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.checkAuthStatus()
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // loggedIn emits a boolean so map it to return true or urltree
+    return this.checkAuthStatus()
+  }
+
+  checkAuthStatus(): Observable<boolean | UrlTree> {
     return this.authService.isLoggedIn$
       .pipe(map(loggedIn => loggedIn ? true : this.router.parseUrl('/login')))
   }
